182f35dd87765a317577cb29c29b9bad93f76ba7
siddharth7061 <siddharth7061@gmail.com>
2025-08-27 15:30:51 +0530

    feat: added services/gca page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import AboutUs from "./pages/about_us";
 import ServicesPage from "./pages/services";
+import GeneralCorporateAdvisory from "./pages/services/gneralCorporateAdvisory";
 
 const App = () => {
   return (
@@ -9,6 +10,10 @@ const App = () => {
       <Route path="/" element={<Home />} />
       <Route path="/aboutus" element={<AboutUs />} />
       <Route path="/services" element={<ServicesPage />} />
+      <Route
+        path="/services/generalcorporateadvisory"
+        element={<GeneralCorporateAdvisory />}
+      />
     </Routes>
   );
 };
